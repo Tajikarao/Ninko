c7a1c3714de4965af872295b3c266ccc640f451c
タジカラオ <103996576+Tajikarao@users.noreply.github.com>
2022-04-27 12:57:06 +0200

    [ADD] fetch all link in a page

--- services/crawler.py
+++ services/crawler.py
@@ -1,13 +1,23 @@
 from services.webresolver import Webresolver
 from utils.database import Database
 
+import httpx
+import re
+
 
 class Crawler:
     def __init__(self) -> None:
         self.webresolver = Webresolver()
         self.database = Database()
+        self.session = httpx.Client(http2=True, headers={'user-agent': 'Ninka'})
+
+    def process_page(self, url):
+        content = self.session.get(url).text
+        urls = re.findall(r'^(?:_|\*\.)?[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$', content)
+        print(urls)
 
     def start(self):
         print(self.database.config.data)
         print(self.database.tld)
         print(self.database.tld_infos)
+        self.process_page("https://nyaa.si/")
